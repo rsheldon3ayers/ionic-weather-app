5f5280502bba0e8cf74202e5aba1f94e12fcbac1
Robert Ayers <rsheldon3ayers@gmail.com>
2016-01-13 10:15:16 -0600

    update ionic weather app

--- www/js/app.js
+++ www/js/app.js
@@ -22,3 +22,34 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+.controller('weatherCtrl', function($http) {
+  var weather = this;
+
+
+  navigator.geolocation.getCurrentPosition(function (geopos) { 
+    console.log(geopos);
+    var lat = geopos.coords.latitude;
+    var long = geopos.coords.longitude;
+    var apiKey = 'b9a64003f5c223789f47fdf82126c69d'; 
+    var url = '/api/forecast/' + apiKey + '/' + lat + ',' + long;
+    
+    $http.get(url).then(function(res) {
+      
+      weather.temp = Math.round(res.data.currently.temperature);
+      console.log("Temp", res, weather.temp);
+    })
+  });
+
+  weather.temp = "--"
+  
+})
+// .config(function ($stateProvider, $urlRouterProvider) {
+
+//   $stateProvider.state("root",{
+//     url: "/",
+//     template: "<h1>Hello World</h1>"
+//   })
+
+//   // $urlRouterProvider.otherwise("/");
+// });
+
